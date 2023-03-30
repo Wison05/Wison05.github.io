cef3382602d8392817a2d1cfd9047b5e785ce39f
Wison05 <92438146+Wison05@users.noreply.github.com>
2023-03-30 15:47:56 +0900

    first homepage

---
+++ script/jquery.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function(){
+    $(".main>li").mouseover(function(){
+        $(".sub").stop().slideDown()
+        $(".box").stop().slideDown()
+    }).mouseout(function(){
+        $(".sub").stop().slideUp()
+        $(".box").stop().slideUp()
+    })
+})
